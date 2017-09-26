3521e5507f9b87d5b83ef58e0d6630efaf106113
szymonides123 <szymek283@gmail.com>
2017-09-26 20:50:40 +0200

    pobieranie firm nauka jazdy

--- dupa.php
+++ dupa.php
@@ -1,25 +1,33 @@
 <?php
 
-$ch = curl_init();
+require('simple_html_dom.php');
 
-$url = 'https://jsonplaceholder.typicode.com';
+$html = file_get_html('https://panoramafirm.pl/kursy_i_nauka_jazdy/firmy,1.html');
+// $dom = str_get_html('https://panoramafirm.pl/kursy_i_nauka_jazdy/firmy,1.html');
 
-$tablica_parametrow = [
-	'id' => 59
-];
-$parametry_get = http_build_query($tablica_parametrow);
+$nazwa = $html->find('a[class=business-card-title addax addax-cs_hl_hit_company_name_click]');
+$miasto = $html->find('div[class=address-container has-left-icon]');
+$kontakty = $html->find('li[class=bottom-bar-tooltip-toggle col-xs]');
+preg_match_all('/\d\d\d\s\d\d\d\s\d\d\d|\d\d\s\d\d\d\s\d\d\s\d\d/', implode(" ", $kontakty), $tel);
+$www = $html->find('a[class=addax addax-cs_hl_hit_homepagelink_click]');
+$email = $html ->find('a[class=addax addax-cs_hl_email_submit_click count-hovers]');
+// echo '<pre>';
+// print_r($tel);
+// die;
+$ilosc = count($nazwa);
+for ($i=0; $i < $ilosc; $i++ ){
 
-$url .= '/posts?' . $parametry_get;
+	echo $nazwa[$i]->plaintext;
+	echo '; ';
+	echo $miasto[$i]->plaintext;
+	echo '; ';
+	echo $tel[0][$i];
+	echo '; ';
+	echo $www[$i]->href;
+	echo '; ';
+	$em_ail = str_replace('mailto:', '', $email[$i]->href);
+	echo $em_ail;
+	echo '</br>';
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+}
 
-$rezultat = curl_exec($ch);
-
-curl_close($ch);
-
-$rezultat = json_decode($rezultat);
-
-echo '<pre>';
-var_dump($rezultat[0]->title);
